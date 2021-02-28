1a0982f85c2bff43f4783e2f209b34d658393703
Your Name <you@example.com>
2021-02-28 17:59:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sgrticket/ui/Login.kt
@@ -0,0 +1,12 @@
+package com.example.sgrticket.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sgrticket.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
